2512580b7bc123566daa5fae30d7b392751b9d7d
David Albert <david@unboared.com>
2023-05-29 15:58:11 +0200

    feat: tron v0.1

--- tron.js
+++ tron.js
@@ -1,231 +1,304 @@
-const canvas = document.getElementById("tron");
-const context = canvas.getContext("2d");
-const unit = 15;
-
 class Player {
-  constructor(x, y, color) {
-    this.color = color || "#fff";
-    this.dead = false;
-    this.direction = "";
-    this.key = "";
-    this.x = x;
-    this.y = y;
-    this.startX = x;
-    this.startY = y;
+        constructor(x, y, direction, color, username) {
+                this.color = color || "#fff";
+                this.username = username || "AI"
+                this.dead = false;
+                this.direction = "";
+                this.key = direction;
+                this.x = x;
+                this.y = y;
+                this.startX = x;
+                this.startY = y;
+                this.startDirection = direction;
 
-    this.constructor.counter = (this.constructor.counter || 0) + 1;
-    this._id = this.constructor.counter;
-
-    Player.allInstances.push(this);
-  }
+                this.constructor.counter = (this.constructor.counter || 0) + 1;
+                this._id = this.constructor.counter;
+        }
 }
 
-Player.allInstances = [];
 
-let p1 = new Player(unit * 6, unit * 6, "#75A4FF");
-let p2 = new Player(unit * 43, unit * 43, "#FF5050");
+class Tron {
+        static UNIT = 15;
+        static MIN_NUM_PLAYER = 2;
+        static MAX_NUM_PLAYER = 4;
+        static REFRESH_TIMELAPSE = 100;
 
-function setKey(key, player, up, right, down, left) {
-  switch (key) {
-    case up:
-      if (player.direction !== "DOWN") {
-        player.key = "UP";
-      }
-      break;
-    case right:
-      if (player.direction !== "LEFT") {
-        player.key = "RIGHT";
-      }
-      break;
-    case down:
-      if (player.direction !== "UP") {
-        player.key = "DOWN";
-      }
-      break;
-    case left:
-      if (player.direction !== "RIGHT") {
-        player.key = "LEFT";
-      }
-      break;
-    default:
-      break;
-  }
-}
+        constructor(width, height) {
+                this.canvas = document.getElementById("tron");
+                this.context = this.canvas.getContext("2d");
 
-function handleKeyPress(event) {
-  let key = event.keyCode;
-
-  if (key === 37 || key === 38 || key === 39 || key === 40) {
-    event.preventDefault();
-  }
-
-  setKey(key, p1, 38, 39, 40, 37); // arrow keys
-  setKey(key, p2, 87, 68, 83, 65); // WASD
-  // setKey(key, p3, 73, 76, 75, 74); // IJKL
-  // setKey(key, p4, 104, 102, 101, 100); // numpad 8456
-}
-
-document.addEventListener("keydown", handleKeyPress);
-
-function getPlayableCells(canvas, unit) {
-  let playableCells = new Set();
-  for (let i = 0; i < canvas.width / unit; i++) {
-    for (let j = 0; j < canvas.height / unit; j++) {
-      playableCells.add(`${i * unit}x${j * unit}y`);
-    }
-  }
-  return playableCells;
-}
-
-let playableCells = getPlayableCells(canvas, unit);
-
-function drawBackground() {
-  context.strokeStyle = "#001900";
-  for (let i = 0; i <= canvas.width / unit + 2; i += 2) {
-    for (let j = 0; j <= canvas.height / unit + 2; j += 2) {
-      context.strokeRect(0, 0, unit * i, unit * j);
-    }
-  }
-
-  context.strokeStyle = "#000000";
-  context.lineWidth = 2;
-  for (let i = 1; i <= (canvas.width + unit) / unit; i += 2) {
-    for (let j = 1; j <= (canvas.height + unit) / unit; j += 2) {
-      context.strokeRect(0, 0, unit * i, unit * j);
-    }
-  }
-  context.lineWidth = 1;
-}
-
-drawBackground();
-
-function drawStartingPositions(players) {
-  players.forEach((p) => {
-    context.fillStyle = p.color;
-    context.fillRect(p.x, p.y, unit, unit);
-    context.strokeStyle = "black";
-    context.strokeRect(p.x, p.y, unit, unit);
-  });
-}
-
-drawStartingPositions(Player.allInstances);
-
-let outcome,
-  winnerColor,
-  playerCount = Player.allInstances.length;
-
-function draw() {
-  if (Player.allInstances.filter((p) => !p.key).length === 0) {
-    if (playerCount === 1) {
-      const alivePlayers = Player.allInstances.filter((p) => p.dead === false);
-      outcome = `Player ${alivePlayers[0]._id} wins!`;
-      winnerColor = alivePlayers[0].color;
-    } else if (playerCount === 0) {
-      outcome = "Draw!";
-    }
-
-    if (outcome) {
-      createResultsScreen(winnerColor);
-      clearInterval(game);
-    }
-
-    Player.allInstances.forEach((p) => {
-      if (p.key) {
-        p.direction = p.key;
-
-        context.fillStyle = p.color;
-        context.fillRect(p.x, p.y, unit, unit);
-        context.strokeStyle = "black";
-        context.strokeRect(p.x, p.y, unit, unit);
-
-        if (!playableCells.has(`${p.x}x${p.y}y`) && p.dead === false) {
-          p.dead = true;
-          p.direction = "";
-          playerCount -= 1;
+                this.initUnboared()
+                this.initGame(width, height)
         }
 
-        playableCells.delete(`${p.x}x${p.y}y`);
-
-        if (!p.dead) {
-          if (p.direction === "LEFT") p.x -= unit;
-          if (p.direction === "UP") p.y -= unit;
-          if (p.direction === "RIGHT") p.x += unit;
-          if (p.direction === "DOWN") p.y += unit;
+        initUnboared() {
+                this.unboared = new Unboared.Game()
+                this.unboared.init()
+                this.unboared.onReady(() => this.resetGame())
+                this.unboared.onConnect(() => this.resetGame())
+                this.unboared.onDisconnect(() => this.resetGame())
+                this.unboared.onMessageReceived((message, from, data) => this.messageHandler(
+                        message, from, data))
+                this.unboared.start()
         }
-      }
-    });
-  }
-}
 
-let game = setInterval(draw, 100);
+        initGame(width, height) {
+                // Start coordinates
+                const start_h = Math.round(0.01 * width) * Tron.UNIT
+                const end_h = width - start_h
+                const start_v = Math.round(0.01 * height) * Tron.UNIT
+                const end_v = height - start_v
 
-function createResultsScreen(color) {
-  const resultNode = document.createElement("div");
-  resultNode.id = "result";
-  resultNode.style.color = color || "#fff";
-  resultNode.style.position = "fixed";
-  resultNode.style.top = 0;
-  resultNode.style.display = "grid";
-  resultNode.style.gridTemplateColumns = "1fr";
-  resultNode.style.width = "100%";
-  resultNode.style.height = "100vh";
-  resultNode.style.justifyContent = "center";
-  resultNode.style.alignItems = "center";
-  resultNode.style.background = "#00000088";
+                // Initialize players start coordinates
+                this.startXY = [
+                        [start_h, start_v, "DOWN"],
+                        [end_h, end_v, "UP"],
+                        [end_h, start_v, "LEFT"],
+                        [start_h, end_v, "RIGHT"]
+                ];
 
-  const resultText = document.createElement("h1");
-  resultText.innerText = outcome;
-  resultText.style.fontFamily = "Bungee, cursive";
-  resultText.style.textTransform = "uppercase";
+                // Initialize informations
+                this.outcome = ""
+                this.winnerColor = ""
+                this.game = null
 
-  const replayButton = document.createElement("button");
-  replayButton.innerText = "Replay (Enter)";
-  replayButton.style.fontFamily = "Bungee, cursive";
-  replayButton.style.textTransform = "uppercase";
-  replayButton.style.padding = "10px 30px";
-  replayButton.style.fontSize = "1.2rem";
-  replayButton.style.margin = "0 auto";
-  replayButton.style.cursor = "pointer";
-  replayButton.onclick = resetGame;
+                // Initialize players
+                this.players = []
+                this.playerCount = 0;
 
-  resultNode.appendChild(resultText);
-  resultNode.appendChild(replayButton);
-  document.querySelector("body").appendChild(resultNode);
+                // Initialize cells
+                this.playableCells = this.getPlayableCells(this.canvas, Tron.UNIT);
 
-  document.addEventListener("keydown", (e) => {
-    let key = event.keyCode;
-    if (key == 13 || key == 32 || key == 27 || key == 82) resetGame();
-  });
-}
+        }
 
-function resetGame() {
-  // Remove the results node
-  const result = document.getElementById("result");
-  if (result) result.remove();
+        updateActivePlayers() {
+                const activeGP = this.unboared.getGamepadIDs().slice(0, Tron.MAX_NUM_PLAYER);
+                this.unboared.loadActivePlayers(activeGP)
+                this.players = []
+                for (let i = 0; i < activeGP.length; i++) {
+                        let color = this.unboared.getColor(activeGP[i])
+                        let username = this.unboared.getUsername(activeGP[i])
+                        let player = new Player(this.startXY[i][0], this.startXY[i]
+                                [1],
+                                this.startXY[i][2], color, username)
+                        this.players.push(player)
+                }
+                this.playerCount = this.players.length;
+                // Reset outcome
+                this.outcome = "";
+                this.winnerColor = "";
+        }
 
-  // Remove background then re-draw it
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  drawBackground();
+        messageHandler(message, from, data) {
+                let activePlayer = this.unboared.getActivePlayerIndex(from)
+                if (activePlayer < this.getNumPlayers()) {
+                        let player = this.players[activePlayer]
+                        switch (message) {
+                                case "UP":
+                                        if (player.direction !== "DOWN")
+                                                player.key = "UP";
+                                        break;
+                                case "RIGHT":
+                                        if (player.direction !== "LEFT") {
+                                                player.key = "RIGHT";
+                                        }
+                                        break;
+                                case "DOWN":
+                                        if (player.direction !== "UP") {
+                                                player.key = "DOWN";
+                                        }
+                                        break;
+                                case "LEFT":
+                                        if (player.direction !== "RIGHT") {
+                                                player.key = "LEFT";
+                                        }
+                                        break;
+                                default:
+                                        break;
+                        }
+                }
 
-  // Reset playableCells
-  playableCells = getPlayableCells(canvas, unit);
+        }
 
-  // Reset players
-  Player.allInstances.forEach((p) => {
-    p.x = p.startX;
-    p.y = p.startY;
-    p.dead = false;
-    p.direction = "";
-    p.key = "";
-  });
-  playerCount = Player.allInstances.length;
-  drawStartingPositions(Player.allInstances);
+        getNumPlayers() {
+                return this.players.length
+        }
 
-  // Reset outcome
-  outcome = "";
-  winnerColor = "";
 
-  // Ensure draw() has stopped, then re-trigger it
-  clearInterval(game);
-  game = setInterval(draw, 100);
+        resetGame() {
+                if (this.game === null) {
+                        // Remove the results node
+                        const result = document.getElementById("result");
+                        if (result) result.remove();
+
+                        this.updateActivePlayers()
+
+                        // Remove background then re-draw it
+                        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                        this.drawBackground();
+
+                        // Reset playableCells
+                        this.playableCells = this.getPlayableCells(this.canvas, Tron.UNIT);
+
+                        // Reset players
+                        this.players.forEach((p) => {
+                                p.x = p.startX;
+                                p.y = p.startY;
+                                p.dead = false;
+                                p.direction = "";
+                                p.key = p.startDirection;
+                        });
+                        this.playerCount = this.players.length;
+                        this.drawStartingPositions(this.players);
+
+                        // Reset outcome
+                        this.outcome = "";
+                        this.winnerColor = "";
+
+                        // Ensure draw() has stopped, then re-trigger it
+                        clearInterval(this.game);
+                        this.game = setInterval(() => this.draw(), Tron.REFRESH_TIMELAPSE);
+                }
+        }
+
+
+        draw() {
+                if (this.getNumPlayers() < Tron.MIN_NUM_PLAYER) {
+                        this.outcome = this.getNumPlayers() === 1 ?
+                                "Requires one additional player!" :
+                                "Requires two additional players!";
+                } else {
+                        if (this.playerCount === 1) {
+                                const alivePlayers = this.players.filter((p) => !p.dead);
+                                this.outcome = `${alivePlayers[0].username} wins!`;
+                                this.winnerColor = alivePlayers[0].color;
+                        } else if (this.playerCount === 0) {
+                                this.outcome = "Draw!";
+                        }
+                }
+
+                if (this.outcome) {
+                        this.createResultsScreen(this.winnerColor);
+                        clearInterval(this.game);
+                        this.game = null
+                }
+
+                this.players.forEach((p) => {
+                        if (p.key) {
+                                p.direction = p.key;
+
+                                this.context.fillStyle = p.color;
+                                this.context.fillRect(p.x,
+                                        p.y, Tron.UNIT, Tron.UNIT);
+                                this.context.strokeStyle = "black";
+                                this.context.strokeRect(p.x, p.y, Tron.UNIT,
+                                        Tron.UNIT);
+
+                                if (!this.playableCells.has(
+                                                `${p.x}x${p.y}y`) && !p.dead) {
+                                        p.dead = true;
+                                        p.direction = "";
+                                        this.playerCount -= 1;
+                                }
+
+                                this.playableCells.delete(
+                                        `${p.x}x${p.y}y`
+                                );
+
+                                if (!p.dead) {
+                                        if (p.direction === "LEFT")
+                                                p.x -= Tron.UNIT;
+                                        if (p.direction === "UP")
+                                                p.y -= Tron.UNIT;
+                                        if (p.direction === "RIGHT")
+                                                p.x += Tron.UNIT;
+                                        if (p.direction === "DOWN")
+                                                p.y += Tron.UNIT;
+                                }
+                        }
+                });
+        }
+
+        drawBackground() {
+                this.context.strokeStyle = "#001900";
+                for (let i = 0; i <= this.canvas.width / Tron.UNIT + 2; i += 2) {
+                        for (let j = 0; j <= this.canvas.height / Tron.UNIT + 2; j += 2) {
+                                this.context.strokeRect(0, 0, Tron.UNIT * i, Tron.UNIT * j);
+                        }
+                }
+
+                this.context.strokeStyle = "#000000";
+                this.context.lineWidth = 2;
+                for (let i = 1; i <= (this.canvas.width + Tron.UNIT) / Tron.UNIT; i += 2) {
+                        for (let j = 1; j <= (this.canvas.height + Tron.UNIT) / Tron.UNIT; j +=
+                                2) {
+                                this.context.strokeRect(0, 0, Tron.UNIT * i, Tron.UNIT * j);
+                        }
+                }
+                this.context.lineWidth = 1;
+        }
+
+        drawStartingPositions(players) {
+                players.forEach((p) => {
+                        this.context.fillStyle = p.color;
+                        this.context.fillRect(p.x, p.y,
+                                Tron.UNIT, Tron.UNIT
+                        );
+                        this.context.strokeStyle = "black";
+                        this.context.strokeRect(p.x, p.y,
+                                Tron.UNIT, Tron.UNIT
+                        );
+                });
+        }
+
+        getPlayableCells(canvas, unit) {
+                let playableCells = new Set();
+                for (let i = 0; i < canvas.width / unit; i++) {
+                        for (let j = 0; j < canvas.height / unit; j++) {
+                                playableCells.add(`${i * unit}x${j * unit}y`);
+                        }
+                }
+                return playableCells;
+        }
+
+        createResultsScreen(color) {
+                const resultNode = document.createElement("div");
+                resultNode.id = "result";
+                resultNode.style.color = color || "#fff";
+                resultNode.style.position = "fixed";
+                resultNode.style.top = 0;
+                resultNode.style.display = "grid";
+                resultNode.style.gridTemplateColumns = "1fr";
+                resultNode.style.width = "100%";
+                resultNode.style.height = "100vh";
+                resultNode.style.justifyContent = "center";
+                resultNode.style.alignItems = "center";
+                resultNode.style.background = "#00000088";
+
+                const resultText = document.createElement("h1");
+                resultText.innerText = this.outcome;
+                resultText.style.fontFamily = "Bungee, cursive";
+                resultText.style.textTransform = "uppercase";
+
+                const replayButton = document.createElement("button");
+                replayButton.innerText = "Replay";
+                replayButton.style.fontFamily = "Bungee, cursive";
+                replayButton.style.textTransform = "uppercase";
+                replayButton.style.padding = "10px 30px";
+                replayButton.style.fontSize = "1.2rem";
+                replayButton.style.margin = "0 auto";
+                replayButton.style.cursor = "pointer";
+                replayButton.onclick = () => this.resetGame();
+
+                resultNode.appendChild(resultText);
+                resultNode.appendChild(replayButton);
+                document.querySelector("body").appendChild(resultNode);
+
+                this.unboared.onMessage((message, data) => {
+                        if (message === "START") {
+                                this.resetGame()
+                        }
+                })
+        }
+
 }
